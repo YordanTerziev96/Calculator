63add9d91b05744dd7309e51eb1e57793e996da6
Yordan Terziev <yordan.tezriev96@gmail.com>
2017-12-19 20:26:22 +0200

    Add project files

---
+++ entity/Calculator.java
@@ -0,0 +1,62 @@
+package com.softuni.entity;
+
+public class Calculator {
+    private double leftOperand;
+    private String operator;
+    private double rightOperand;
+
+    public Calculator(double leftOperand, String operator, double rightOperand) {
+        this.leftOperand = leftOperand;
+        this.operator = operator;
+        this.rightOperand = rightOperand;
+    }
+
+    public double getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(double leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    public String getOperand() {
+        return operator;
+    }
+
+    public void setOperand(String operator) {
+        this.operator = operator;
+    }
+
+    public double getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(double rightOperand) {
+        this.rightOperand = rightOperand;
+    }
+
+    public double calculateResult(){
+        double result;
+        switch (operator){
+            case "+":
+                result = this.getLeftOperand() + this.getRightOperand();
+                break;
+            case "-":
+                result = this.getLeftOperand() - this.getRightOperand();
+                break;
+            case "*":
+                result =  this.getLeftOperand() * this.getRightOperand();
+                break;
+            case "/":
+                result = this.getLeftOperand() / this.getRightOperand();
+                break;
+            case "%":
+                result = this.getLeftOperand() % this.getRightOperand();
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        return result;
+    }
+}
